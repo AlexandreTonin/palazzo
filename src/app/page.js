b7c99c8068b01a5f41c4b688b52d20f2bd2d64e7
Alexandre Tonin <xandetonin@gmail.com>
2025-09-25 21:28:39 -0300

    feat: implement basic structure of Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-   <>
-   </>  
+    <>
+      <section className="h-screen bg-[linear-gradient(0deg,rgba(0,0,0,0.2),rgba(0,0,0,0.2)),radial-gradient(42.32%_51.73%_at_50%_40.77%,#C6C6C6_10%,#88B9D6_100%)]">
+        <div className="absolute h-4/5 w-full bottom-0">
+          <Image
+            src={"/hero-house.png"}
+            alt="Hero House"
+            fill
+            draggable={false}
+            className="object-cover object-top"
+          />
+        </div>
+      </section>
+    </>
   );
 }
