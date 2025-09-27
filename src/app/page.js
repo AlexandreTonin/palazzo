83ede62a1d5ca8f58f251f0346caabcc555736d6
Alexandre Tonin <xandetonin@gmail.com>
2025-09-27 11:29:01 -0300

    fix: remove invalid import

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import { HeroSection } from "@/components/layout/HeroSection";
-import { PropertiesSection } from "@/components/layout/PropertiesSection";
 
 export default function Home() {
   return (
