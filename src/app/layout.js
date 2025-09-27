c5b0e17bfa285307ebdf10b363af63649dc97f77
Alexandre Tonin <xandetonin@gmail.com>
2025-09-27 12:03:05 -0300

    fix: update body tag to include cz-shortcut-listen attribute

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,13 +3,14 @@ import "./globals.css";
 
 export const metadata = {
   title: "Palazzo",
-  description: "We specialize in high-end real estate, offering a curated selection of luxury properties in prime locations. Our mission is to connect you with exclusive homes that combine sophistication, comfort, and exceptional design. Whether you are looking to buy or invest, our portfolio reflects the highest standards of quality and lifestyle.",
+  description:
+    "We specialize in high-end real estate, offering a curated selection of luxury properties in prime locations. Our mission is to connect you with exclusive homes that combine sophistication, comfort, and exceptional design. Whether you are looking to buy or invest, our portfolio reflects the highest standards of quality and lifestyle.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body cz-shortcut-listen="true">
         <Header />
         {children}
       </body>
